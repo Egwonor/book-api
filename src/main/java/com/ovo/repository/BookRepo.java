d0e8f3b801f2d9c5f7e8d24b171d4e9d2ac7856a
ovo <oghenehonour@gmail.com>
2020-08-06 14:14:57 +0200

    initial commit

---
+++ src/main/java/com/ovo/repository/BookRepo.java
@@ -0,0 +1,12 @@
+package com.ovo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ovo.entity.Book;
+
+
+public interface BookRepo extends JpaRepository<Book, Long>{
+	
+	Book findByName(String name);
+
+}
